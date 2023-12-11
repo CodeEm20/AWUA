1fb2c680845e149bba1c55f92ea9b84efdd39915
CodeEm20 <emmacool20@gmail.com>
2023-12-11 15:52:45 +0100

    Code test

--- app/src/main/java/com/example/awua/MainActivity.java
+++ app/src/main/java/com/example/awua/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.awua;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.Session;
+import ch.ethz.ssh2.StreamGobbler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,5 +48,52 @@ public class MainActivity extends AppCompatActivity {
         txv_alarmSound = (TextView) findViewById(R.id.txv_alarmSound);
         btn_newAlarm = (Button) findViewById(R.id.btnNew);
         btn_musicFile = (Button) findViewById(R.id.btnFile);
+
+        btn_musicFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                run("Python/PlaySound.py");
+// below you write code to change switch status and action to take
+            }
+        });
     }
+
+    public void run (String command) {
+        String hostname = "raspberrypi";
+        String username = "pi";
+        String password = "raspberry";
+        try
+        {
+            StrictMode.ThreadPolicy policy = new
+                    StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            Connection conn = new Connection(hostname); //init connection
+            conn.connect(); //start connection to the hostname
+            boolean isAuthenticated = conn.authenticateWithPassword(username,
+                    password);
+            if (isAuthenticated == false)
+                throw new IOException("Authentication failed.");
+            Session sess = conn.openSession();
+            sess.execCommand(command);
+            InputStream stdout = new StreamGobbler(sess.getStdout());
+            BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
+//reads text
+            while (true){
+                String line = br.readLine(); // read line
+                if (line == null)
+                    break;
+                System.out.println(line);
+            }
+            /* Show exit status, if available (otherwise "null") */
+            System.out.println("ExitCode: " + sess.getExitStatus());
+            sess.close(); // Close this session
+            conn.close();
+        }
+        catch (IOException e)
+        { e.printStackTrace(System.err);
+            System.exit(2); }
+    }
+
 }
